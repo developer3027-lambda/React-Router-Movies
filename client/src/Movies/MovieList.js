1f6f1392f2191dc42fc43c43837a3de3979823dd
Mason Roberts <mrobe18@gmail.com>
2019-09-18 02:41:49 -0400

    did something

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -30,7 +31,9 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    
     <div className="movie-card">
+    <Link to={`/movies/${movie.id}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -39,11 +42,12 @@ function MovieDetails({ movie }) {
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
-
+      </Link>
       {stars.map(star => (
         <div key={star} className="movie-star">
           {star}
         </div>
+        
       ))}
     </div>
   );
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+
+function Home(props) {
+  const routeToHome = event => {
+    event.preventDefault();
+    props.history.push('./');
+  }
+}
+
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <button onClick={Home} className="home-button">Home</button>
   </div>
 );
 
